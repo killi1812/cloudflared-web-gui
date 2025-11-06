9369a523a1e787517ef2574372da54e270ef60e4
killi1812 <cvok.fran@gmail.com>
2025-11-06 12:08:52 +0100

    New task run

--- server/app/var.go
+++ server/app/var.go
@@ -9,29 +9,16 @@ var (
 	// Build describes app build type
 	//
 	// Could be: dev, prod
-	Build = BuildDev
-	// Version is a semver version of the app
-	Version = "0.0.0"
-	// CommitHash is latest build commit hash
-	CommitHash = "n/a"
-	// BuildTimestamp stores when the app was build
-	BuildTimestamp = "n/a"
+	Build          = BuildDev
+	Version        = "0.0.0" // Version is a semver version of the app
+	CommitHash     = "n/a"   // CommitHash is latest build commit hash
+	BuildTimestamp = "n/a"   // BuildTimestamp stores when the app was build
 )
 
 // Envirment variables
 
 var (
 	Port       int    // Port is app port
-	DbConn     string // Postgress Connection string
 	AccessKey  string // AccessKey is secrete for jwt access key
 	RefreshKey string // RefreshKey is secrete for jwt refresh key
-	MongoConn  string // MongoConn is mongo db connection string
-)
-
-// MinIO connection configuration
-var (
-	MIOEndpoint        string
-	MIOAccessKeyID     string
-	MIOSecretAccessKey string
-	MIOUseSSL          bool
 )
